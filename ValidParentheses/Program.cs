f5cdea81c8d9e54db8ff2fb94ece04f1e75463f6
fasdser <107477645+fasdser@users.noreply.github.com>
2022-10-15 14:25:05 +0300

    Add project files.

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParentheses("hi(hi)"));
+            Console.ReadKey();
+        }
+
+        public static bool ValidParentheses(string input)
+        {
+            int parentheses = 0;
+            foreach (char t in input)
+            {
+                if (t == '(')
+                {
+                    parentheses++;
+                }
+                else if (t == ')')
+                {
+                    parentheses--;
+
+                    if (parentheses < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return parentheses == 0;
+        }
+
+        public static bool ValidParentheses1(string input)
+        {
+            input = string.Concat(input.Where(x => "()".Contains(x)));
+            while (input.Contains("()"))
+            {
+                input = input.Replace("()", "");
+            }
+
+            return !input.Any();
+        }
+    }
+}
